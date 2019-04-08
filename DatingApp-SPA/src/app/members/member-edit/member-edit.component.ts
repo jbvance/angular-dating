15cd44ea796a6f821a5cbea07280c51fe41af8e3
Jason Vance <=>
2019-04-08 11:43:09 -0500

    complete member edit componenet

--- DatingApp-SPA/src/app/members/member-edit/member-edit.component.ts
+++ DatingApp-SPA/src/app/members/member-edit/member-edit.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit, ViewChild, HostListener } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { User } from './../../_models/user';
-import { AlertifyService } from './../../_services/alertify.service';
 import { NgForm } from '@angular/forms';
-
+import { AlertifyService } from './../../_services/alertify.service';
+import { UserService } from './../../_services/user.service';
+import { User } from './../../_models/user';
+import { AuthService } from './../../_services/auth.service';
 @Component({
   selector: 'app-member-edit',
   templateUrl: './member-edit.component.html',
@@ -21,7 +22,10 @@ export class MemberEditComponent implements OnInit {
     }
   }
 
-  constructor(private route: ActivatedRoute, private alertify: AlertifyService) { }
+  constructor(private route: ActivatedRoute,
+              private alertify: AlertifyService,
+              private userService: UserService,
+              private authService: AuthService) { }
 
   ngOnInit() {
     this.route.data.subscribe(data => {
@@ -31,8 +35,13 @@ export class MemberEditComponent implements OnInit {
 
   updateUser() {
     console.log(this.user);
-    this.alertify.success('Profile updated successfully');
-    this.editForm.reset(this.user);
+    this.userService.updateUser(this.authService.decodedToken.nameid, this.user).subscribe(next => {
+      this.alertify.success('Profile updated successfully');
+      this.editForm.reset(this.user);
+    }, error => {
+      this.alertify.error('Error Saving User');
+      console.log('ERROR SAVING USER', error);
+    });
   }
 
 }
